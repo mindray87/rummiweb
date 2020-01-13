e45344ee8eba4b5f3a7fbd6c68794dbbc0211be4
Julian R <julianriegraf@web.de>
2020-01-13 20:25:46 +0100

    added vue frontend

---
+++ VueFrontend/src/components/TileRow.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <TileElement
+                v-for="tile in tiles"
+                v-bind:key="tile.id"
+                v-bind:number="tile.number"
+                v-bind:color="tile.color"
+        ></TileElement>
+    </div>
+</template>
+
+<script>
+    import TileElement from './TileElement.vue';
+
+    export default {
+        name: "TileRow",
+        components: {
+            TileElement
+        },
+        data: function () {
+            return {
+                tiles: [
+                    {id: 1, number: 1, color: "green"},
+                    {id: 2, number: 2, color: "red"},
+                    {id: 3, number: 3, color: "yellow"}
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
